97ba84f4d0ac0a0e8beccd2e9113d2549175888c
Ruth Bhatti <168582855+RuthBhatti@users.noreply.github.com>
2024-07-08 21:15:17 -0400

    Resolve merge conflicts

--- routes/vetForm.js
+++ routes/vetForm.js
@@ -4,16 +4,6 @@ import fs from 'fs';
 const router = express.Router();
 const VET_FORM_FILE = './data/vetForms.json';
 
-// // Function to read data from the file
-// const readData = () => {
-//     if (!fs.existsSync(VET_FORM_FILE)) {
-//         return [];
-//     }
-//     const rawData = fs.readFileSync(VET_FORM_FILE);
-//     return JSON.parse(rawData);
-// };
-
-// Function to write data to the file
 const writeData = (data) => {
     fs.writeFileSync(VET_FORM_FILE, JSON.stringify(data, null, 2));
 };
@@ -21,9 +11,6 @@ const writeData = (data) => {
 router.post('/', (req, res) => {
     const formData = req.body;
     console.log(formData)
-    // const vetForms = readData();
-
-    // vetForms.push(formData);
     writeData(formData);
 
     res.status(200).json({ message: 'Veterinary Health Certificate submitted successfully', data: formData });
